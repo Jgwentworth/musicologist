901b047427236e22e684541ce74fd63e7500f401
Jared George <jaredgeorge.us@gmail.com>
2017-08-29 00:52:10 -0700

    added animation

--- music.js
+++ music.js
@@ -4,6 +4,7 @@ $("#artist-error").hide();
 $("#playlist-title").hide();
 $("#similar-artist").hide();
 $("#no-results").hide();
+$("#empty-list").hide();
 
 function addSongFromField(event) {
   event.preventDefault();
@@ -18,6 +19,7 @@ function addSongFromField(event) {
   $("#artist-error").hide();
   renderList();
   $('#musicField').eq(0).val('');
+  changeProgressBar(1);
 }
 //removes song on click
 $('body').on("click", "li.list-group-item", function(){
@@ -27,9 +29,15 @@ $('body').on("click", "li.list-group-item", function(){
   });
   musicInfo = result;
   $(this).remove();
+  $("#getPlaylistBtn").attr("class", "btn btn-default disabled")
+  $("#playlist").empty()
+  changeProgressBar(-1);
+  $("#similar-artist").hide();
+  $("#addSongPrompt").show();
 })
-//TODO: validation for song name
-$('#addButton').click(addSongFromField);
+
+$('#addButton').click(addSongFromField)
+
 $('#musicField').keyup(function(event) {
   if (event.which == 13) { // User presses Enter
     addSongFromField(event);
@@ -49,16 +57,60 @@ function renderList() {
 }
 
 function addSongs(list){
+  //console.log(list);
+  shuffle(list);
+  //console.log(list);
   for (i = 0; i < list.length; i++) {
     let playlistSong = $("<div class='panel panel-default'>");
     playlistSong.append($("<div class='panel-body'>").text(list[i].trackName))
     playlistSong.append($("<div class='panel-footer'>").text("By " + list[i].artistName))
     $("#playlist").append(playlistSong);
-    //$("#playlist").append($("<button type='button' class='list-group-item'>").text(list[i].trackName))
-
   }
 }
-// Displays Playlist
+
+// TODO shuffle function
+function shuffle(array) {
+  console.log(array)
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  console.log(array)
+  return array;
+}
+
+
+let progressBar = 0
+function changeProgressBar(val){
+
+  if (val > 0) {
+    progressBar = progressBar + 20;
+    if (progressBar >= 100) {
+      progressBar = 100;
+      $("#getPlaylistBtn").attr( "class", "btn btn-primary btn-lg");
+      $("#addSongPrompt").hide();
+    }
+  } else {
+    progressBar = progressBar - 20;
+    if (progressBar < 0) {
+      progressBar = 0;
+    };
+  }
+  let size = 'width: ' + progressBar + '%'
+  $("#progress-bar").attr( "style" , size);
+  return progressBar
+};
+
 function displayPlaylist(){
   $("#playlist").empty()
   
@@ -77,14 +129,16 @@ function displayPlaylist(){
       }
     })
     .catch(function (err){
-    $("#no-results").show();  
     console.log ("something went wrong", err)
     });
   };
   }
 
-
 $('#getPlaylistBtn').click(function (event) {
+  if (musicInfo.length == 0) {
+    $("#empty-list").show();
+    return
+  };
   $("#playlist-title").show();
   $("#similar-artist").show();
   // TODO: Display songs with thumbnail on album
